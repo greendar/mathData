2fc759bef35da59384aace945a47cb894d388338
Mr Green <dwgreen@hwdsb.on.ca>
2024-02-26 14:58:26 -0500

    Create student.py
    initial commit

---
+++ student.py
@@ -0,0 +1,16 @@
+class Student:
+  def __init__(self, fName, lName):
+    firstName = fName
+    lastName = lName
+
+
+class Course:
+  def __init__(self, code, teacher, mark, year, sem):
+    courseCode = code
+    courseTeacher = teacher
+    courseMark = mark
+    schoolYear = year
+    semester = sem
+
+if __name__ == "__main__":
+  pass
