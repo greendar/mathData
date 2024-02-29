b5671d86701b348dc43b083738bd3207b2fe1ab1
greendar <dwgreen@hwdsb.on.ca>
2024-02-29 13:11:05 -0500

    new commit

--- csvImporter.py
+++ csvImporter.py
@@ -1,13 +1,22 @@
+from student import *
+
 """
 Need to figure out the format of the CSV files to be loaded in.
-
+Code ==> [0]
+Teacher ==> [1]
+Mark ==> [2]
+Year ==> [3]
+Semester ==>[4]
 """
 
+newStudent = Student('Bob', 'Ross')
 
 with open("Book1.csv") as file_in:
     lines = []
     for line in file_in:
-        print(line)
-        lines.append(line)
+        dataList = line.split(",")
+        newStudent.newCourse = Course(dataList[0], dataList[1], dataList[2], dataList[3], dataList[4])
+        #print(newStudent.newCourse)
+        #lines.append(line)
 
-print(lines)
+print(newStudent)
--- student.py
+++ student.py
@@ -1,20 +1,42 @@
 class Student:
   def __init__(self, fName, lName):
-    firstName = fName
-    lastName = lName
-    courses = []
+    self.firstName = fName
+    self.lastName = lName
+    self.courses = []
   
   def addClass(self, newCourse):
     self.courses.append(newCourse)
 
+  def __str__(self):
+    courseList = f"{self.firstName} {self.lastName}"
+    for item in self.courses:
+      courseList += f'Code: {self.courseCode}    '
+      courseList += f'Teacher: {self.courseTeacher}    '
+      courseList += f'Mark: {self.courseMark}    '
+      courseList += f'Year: {self.schoolYear}    '
+      courseList += f'Semester: {self.semester}    '
+      return courseList
+  
+
 
 class Course:
   def __init__(self, code, teacher, mark, year, sem):
-    courseCode = code
-    courseTeacher = teacher
-    courseMark = mark
-    schoolYear = year
-    semester = sem
+    self.courseCode = code
+    self.courseTeacher = teacher
+    self.courseMark = mark
+    self.schoolYear = year
+    self.semester = sem
+
+  def __str__(self):
+    outString = ''
+    outString += f'Code: {self.courseCode}    '
+    outString += f'Teacher: {self.courseTeacher}    '
+    outString += f'Mark: {self.courseMark}    '
+    outString += f'Year: {self.schoolYear}    '
+    outString += f'Semester: {self.semester}    '
+    return outString
+
+
 
 if __name__ == "__main__":
   print("i ran")
